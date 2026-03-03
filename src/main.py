ef8519023df42156d6abc34f02ecceef28aead90
Nirika Lamichhane <nirikalamichhane12@gmail.com>
2026-03-03 15:14:23 +0545

    checked the accuracy

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 # src/main.py
 import numpy as np
-from sklearn.linear_model import LogisticRegression
+from logistic_regression import logisticregression
 from data_utils import generate_3d_Datas
 from pca import center_data, covariance_matrix, eiggen_decompostition, final_projection
+import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D
 
-X,y = generate_3d_Datas()
+
+X, y = generate_3d_Datas()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X[y == 0][:, 0], X[y == 0][:, 1], X[y == 0][:, 2]) # this is of class 0
+ax.scatter(X[y == 1][:, 0], X[y == 1][:, 1], X[y == 1][:, 2]) # this is of class 1
+plt.show()
+# [:, 0] means we are taking all the rows and only the first column, [:, 1] means we are taking all the rows and only the second column and [:, 2] means we are taking all the rows and only the third column. This is how we are plotting the 3D scatter plot for both classes.
 
 X_centered, mean = center_data(X)
 
